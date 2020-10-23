f3ea89245f3d2ed5f895140a48235f2c84a40638
Stephen Roddy <roddyst@tcd.ie>
2020-10-23 16:19:01 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let sound,samhain, sW, sH, lake, fft, glitch, glitch2,analyzer;
+let sound,samhain, sW, sH, lake, fft, glitch, glitch2,analyzer,glScl,glSclMod;
 
 function preload(){
   sound = loadSound('audio/SIGIL.mp3');
@@ -7,94 +7,89 @@ function preload(){
 
   glitch = new Glitch();
   loadImage('/img/samhain.png', function(im){
-  //  glitch.loadType('png'); // complete type as processed
-		glitch.loadImage(im);
-	});
+    glitch.loadQuality(.25); // reduce quality
+    glitch.loadImage(im);
+  });
 
   glitch2 = new Glitch();
-  loadImage('/img/bckScl.png', function(im2){
-    //glitch.loadType('jpg'); // complete type as processed
-		glitch2.loadImage(im2);
-	});
+  loadImage('/img/bckScl.png', function(im){
+        //glitch.loadType('jpg'); // complete type as processed
+  glitch2.loadQuality(.25);
+  glitch2.loadImage(im);
+  });
 }
 
+
 function setup(){
+
   frameRate(10); // Keep frameRate (and fft ins) low to save on overhead
-  imageMode(CENTER);
   let cnv = createCanvas(1000,1000);
-  //image(lake,500,500);
-  //image(samhain,500,500);
-  sound.loop();
-  //sound.pause();
-    // create a new Amplitude analyzer
-  analyzer = new p5.Amplitude();
-  // Patch the input to an volume analyzer
-  analyzer.setInput(sound);
-
-// Create new fft
-  fft = new p5.FFT(.8,16); //16 bins. Small fft. Smoothing factor of .8/default
-  fft.setInput(sound);
-
+  imageMode(CENTER);
+  image(lake,500,500);
+  image(samhain,500,500);
+  initSound();
   cnv.mouseClicked(togglePlay);
-  sound.amp(0.3);
-
 
 }
 
 
 function draw(){
-
   if (sound.isPlaying()) {
-
-    gl_2b();
-    gl_1c();
-
+    analyseSound();
+    gl();
+    gl_2();
+    sigil();
   }
-  else {
-  //  image(lake,500,500);
-//    image(samhain,500,500);
-}
 
 }
 
-// Working glitch function based on amplitude.
 function gl_1c(){
-  let rmsScl = ceil(5000*(analyzer.getLevel()));
-  glitch.resetBytes();
-  glitch.randomBytes((rmsScl)); // randomise bytes on basis of amplitude range output from 1 to 200
 
+  glitch.resetBytes();
+  glitchScl = ((fft.getEnergy("mid"))/256)*1;
+  glitch.randomBytes(glitchScl); // randomise bytes on basis of amplitude range output from 1 to 200
   glitch.buildImage();
   tint(255, 127); // Display at half opacity
   image(glitch.image, width/2, height/2);
-  console.log(rmsScl);
+
 }
 
 
-//working fft driven glitch function
+function gl(){
+  glitch2.resetBytes(); // reset glitch to original bytes if loaded in setup
+  glSclMod = ((fft.getEnergy("mid"))/256)*15;
+  glScl = ((fft.getEnergy("treble"))/256)*glSclMod;
+  glitch2.randomBytes(glScl); // randomize 10 bytes();
+//  tint(255, 33); // Display at low opacity
+  glitch2.buildImage();
+  image(glitch2.image, width/2, height/2);
+}
+
+function gl_2(){
+  glitch2.resetBytes(); // reset glitch to original bytes if loaded in setup
+  glScl = ((fft.getEnergy("highMid"))/256)*5;
+  glitch2.randomBytes(glScl); // randomize 10 bytes();
+  tint(fft.getEnergy("bass"),fft.getEnergy("mid"),fft.getEnergy("treble"), 33 ); // Display at low opacity
+  glitch2.buildImage();
+  image(glitch2.image, width/2, height/2);
+}
+
+
 function gl_2b(){
 
   let spectrum, bass, lowMid, mid, highMid, treble;
-
-  spectrum = fft.analyze();
-
-  // The enrgy levels scale in a range of 0 to 255
   bass = fft.getEnergy("bass");
-  //lowMid = fft.getEnergy("lowMid");
   mid = fft.getEnergy("mid");
-//  highMid = fft.getEnergy("highMid");
-  //treble = fft.getEnergy("treble");
 
   glitch2.resetBytes();
-  //glitch2.replaceBytes(50, bass); // swap all decimal byte 100 for 104
-//  glitch2.replaceBytes(150, lowMid); // swap all decimal byte 100 for 104
   glitch2.replaceBytes(255-bass, mid); // swap all decimal byte 100 for 104
-  //glitch2.replaceBytes(300, highMid); // swap all decimal byte 100 for 104
   glitch2.replaceBytes(5000+highMid, treble); // swap all decimal byte 100 for 104
-
   glitch2.buildImage();
+
   tint(255, 33); // Display at low opacity
   image(glitch2.image, width/2, height/2);
 }
+
 
  function togglePlay() {
    if (sound.isPlaying()) {
@@ -105,3 +100,18 @@ function gl_2b(){
      sound.loop();
    }
  }
+
+ function initSound() {
+   fft = new p5.FFT(0.6,1024);
+   sound.amp(0.7);
+ }
+ function analyseSound() {
+   soundSpectrum = fft.analyze(); // spectrum is array of amplitudes of each frequency
+ }
+
+function sigil(){
+  //rotate(PI / 3.0);
+  tint(255,255);
+  image(samhain, width/2, height/2,125+(1.5*(fft.getEnergy("bass"))),125+(1.5*(fft.getEnergy("bass"))));
+  //samhain.resize(2*(fft.getEnergy("bass")),2*(fft.getEnergy("bass")));
+}
